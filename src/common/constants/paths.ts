93e6a1a943b01097142da7d3998667b6a4d31136
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-07-30 15:06:18 +0200

    add routing paths

---
+++ src/common/constants/paths.ts
@@ -0,0 +1,19 @@
+export enum Paths {
+  // Public
+  INDEX = "/",
+
+  // Auth
+  LOGIN = "/auth/login",
+  REGISTER = "/auth/register",
+
+  // Protected
+  DASHBOARD = "/dashboard",
+  OFFERS = "/offers",
+  CANDIDATES = "/candidates",
+  PIPELINE = "/pipeline",
+  MAILING = "/mailing",
+  STATS = "/stats",
+
+  // Not found
+  NOT_FOUND = "*",
+}
