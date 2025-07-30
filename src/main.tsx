aad41f7628958cb50456aea51c40c478405fff32
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-07-31 00:06:41 +0200

    initialize theme & add styles file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import App from "./App.tsx";
+import { initTheme } from "./common/utils/theme";
+
+import "./common/styles/main.css";
+
+initTheme();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
