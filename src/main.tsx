80e39ed8d4cc635b6dee3fcdfe4b722c67f13ab8
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-08-01 13:54:41 +0200

    add store provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 import { initTheme } from "./common/utils/theme";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 import "./common/styles/main.css";
 
 initTheme();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 );
