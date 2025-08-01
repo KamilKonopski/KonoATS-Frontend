53d84f225c1c81731e4512b8f28fe3af50ceda50
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-08-01 13:57:42 +0200

    change to authenticate from auth slice

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -1,12 +1,15 @@
 import type { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../store/hooks/useAuth";
 
 interface ProtectedRouteProps {
   children: ReactNode;
 }
 
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-  const isAuthenticated = !localStorage.getItem("token");
+  const { isAuthenticated, isAuthLoaded } = useAuth();
+
+  if (!isAuthLoaded) return null;
 
   return isAuthenticated ? children : <Navigate to="/auth/login" replace />;
 };
