137c3024a74a9d79a9d1c3e98a6720102d747339
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-08-01 13:59:55 +0200

    feat: create simple footer component

---
+++ src/common/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      KonoATS v0.1.0 &copy; 2025 Created by{" "}
+      <a href="https://github.com/kamilkonopski">Kamil Konopski</a>
+    </footer>
+  );
+};
+
+export default Footer;
