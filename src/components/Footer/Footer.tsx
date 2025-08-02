130875befabd30dac687c7d8857c6e513fcc96d8
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-08-03 01:03:48 +0200

    remove v0.1.0 from footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <p>
-      KonoATS v0.1.0 &copy; 2025 Created by
+      KonoATS &copy; 2025 Created by
       <a href="https://github.com/kamilkonopski"> Kamil Konopski</a>
     </p>
   );
