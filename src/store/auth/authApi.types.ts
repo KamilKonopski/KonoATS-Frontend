630909d1b6cc1e71bc149ab462db22afaeeb6a82
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-08-07 15:44:09 +0200

    feat: add user, login response & request interface

--- src/store/auth/authApi.types.ts
+++ src/store/auth/authApi.types.ts
@@ -1,6 +1,22 @@
 export type AuthState = {
-  isAuthenticated: boolean;
   token: string | null;
-  error: string | null;
+  isAuthenticated: boolean;
   isAuthLoaded: boolean;
+  error: string | null;
 };
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+  remember: boolean;
+}
+
+export interface LoginResponse {
+  token: string;
+}
+
+export interface User {
+  id: string;
+  email: string;
+  name?: string;
+}
