6172c4bad3f0bc844362adf62ca686004c21d1b1
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-08-19 01:09:20 +0200

    feat: add job offer interface

---
+++ src/services/types/offerts.ts
@@ -0,0 +1,26 @@
+export interface JobOffer {
+  id: string;
+  title: string;
+  description: string;
+  requirements: string[];
+  salaryRange: {
+    min: number;
+    max: number;
+    currency: string;
+  };
+  contractType: string[];
+  location: string;
+  level: "Junior" | "Mid" | "Senior";
+  status: "Otwarta" | "Zamknięta" | "Wstrzymana";
+  language: string;
+  teamSize: string;
+  benefits: string[];
+  technologies: {
+    mustHave: string[];
+    niceToHave: string[];
+  };
+  company: string;
+  createdAt: string;
+  updatedAt: string;
+  candidates: string[];
+}
