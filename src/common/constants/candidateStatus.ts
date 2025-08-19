2fc7cbcd3a542a2d16f9b98a89f97c2beda3710f
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-08-19 12:14:31 +0200

    feat: create candidate status enum

---
+++ src/common/constants/candidateStatus.ts
@@ -0,0 +1,9 @@
+export enum CandidateStatus {
+  NEW = "Nowy",
+  SCREENING = "Screening",
+  INTERVIEW = "Rozmowa",
+  TASK = "Zadanie",
+  OFFERT = "Oferta",
+  REJECTED = "Odrzucony",
+  HIRED = "Zatrudniony",
+}
